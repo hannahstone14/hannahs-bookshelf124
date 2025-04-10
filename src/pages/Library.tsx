5c1e43adc0b1a460f50351ac2a9110ac85837752
Your Name <109406856+hannahstone14@users.noreply.github.com>
2025-04-10 18:00:22 -0400

    Fix: Update import paths for BookshelfStats and Bookshelf components

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BookshelfProvider } from '@/context/BookshelfContext';
-import BookshelfStats from '@/components/library/BookshelfStats';
-import Bookshelf from '@/components/library/Bookshelf';
+import BookshelfStats from '@/components/BookshelfStats';
+import Bookshelf from '@/components/Bookshelf';
 
 const Library = () => {
+  const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
+  
+  const handleAddBookClick = () => {
+    setIsAddDialogOpen(true);
+  };
+  
+  const handleDialogClose = () => {
+    setIsAddDialogOpen(false);
+  };
+  
   return (
     <BookshelfProvider>
       <div className="min-h-screen flex flex-col bg-gray-50">
         <main className="flex-grow py-8 px-4 sm:px-6 lg:px-8">
           <div className="max-w-6xl mx-auto">
-            <BookshelfStats />
-            <Bookshelf />
+            <BookshelfStats onAddBookClick={handleAddBookClick} />
+            <Bookshelf 
+              isAddDialogOpen={isAddDialogOpen} 
+              onDialogClose={handleDialogClose} 
+              onAddBookClick={handleAddBookClick} 
+            />
           </div>
         </main>
       </div>
